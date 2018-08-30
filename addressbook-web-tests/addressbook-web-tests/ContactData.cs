25d311afb990c9bbab85b40e599e838423378bc0
WORK/user <alexfebenchukov@gmail.com>
2018-08-30 15:42:55 +0700

    Добавление нового контакта.

---
+++ addressbook-web-tests/addressbook-web-tests/ContactData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressBookTests
+{
+    class ContactData
+    {
+        private string name = "";
+        private string sureName = "";
+        private string lastName = "";
+
+        public ContactData(string name)
+        {
+            this.name = name;
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string SureName
+        {
+            get { return sureName; }
+            set { sureName = value; }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+    }
+}
